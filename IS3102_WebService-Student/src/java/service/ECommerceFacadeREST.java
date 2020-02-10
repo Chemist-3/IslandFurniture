2314db70dfbfb71d4f1678964d57e177b9e4406b
EEFREY <38551343+Snowdrifft@users.noreply.github.com>
2020-02-10 12:47:21 +0800

    added payment ws

--- IS3102_WebService-Student/src/java/service/ECommerceFacadeREST.java
+++ IS3102_WebService-Student/src/java/service/ECommerceFacadeREST.java
@@ -1,11 +1,6 @@
 package service;
 
-import java.net.URI;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.Date;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
@@ -15,8 +10,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import model.dbaccess;
 
 @Path("commerce")
 public class ECommerceFacadeREST {
@@ -27,6 +22,9 @@ public class ECommerceFacadeREST {
     public ECommerceFacadeREST() {
     }
 
+    private dbaccess db = new dbaccess();
+    
+    
     @GET
     @Produces("application/json")
     public String getJson() {
@@ -43,5 +41,85 @@ public class ECommerceFacadeREST {
     @PUT
     @Consumes("application/json")
     public void putJson(String content) {
+    }
+
+    @GET
+    @Path("createECommerceTransactionRecord")
+    @Produces("application/json")
+    public Response createECommerceTransactionRecord(@QueryParam("memberId") Long memberID, 
+            @QueryParam("amountPaid") Double amountPaid, @QueryParam("countryID") Long countryID) {
+        
+        System.out.println("RESTful: createECommerceTransactionRecord() called with memberID=" + memberID + "  amountPaid=" + amountPaid + " and countryID=" + countryID);
+        
+        try {
+            // instantiate variables
+            String currency = "";
+            String storeID = "";
+            java.util.Date dt = new java.util.Date();
+            java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String currentTime = sdf.format(dt);
+            
+            // retrieve storeID and currency
+            ResultSet rs = db.getCountryStoreIDAndCurrency(countryID);
+            while (rs.next()) {
+                currency = rs.getString("currency");
+                storeID = rs.getString("storeID");
+            }
+            // error checking
+            if (currency.equals("") || storeID.equals("")) {
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
+
+            // create new entry in salesrecordentity and retrieve salesRecordID
+            Long generatedKey = db.insertSalesrecordentity(amountPaid, currentTime, currency, (new Date()).getTime(), memberID, Integer .parseInt(storeID));
+            
+            // error checking
+            if (generatedKey > 0L) {
+                return Response.status(Response.Status.CREATED).entity(generatedKey + "").build();
+            } else {
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+
+    @GET
+    @Path("createECommerceLineItemRecord")
+    @Produces({"application/json"})
+    public Response createECommerceLineItemRecord(@QueryParam("salesRecordID") String salesRecordID, @QueryParam("itemID") String itemID, 
+            @QueryParam("quantity") int quantity, @QueryParam("countryID") Long countryID) {
+        
+        System.out.println("RESTful: createECommerceLineItemRecord() called with salesRecordID=" + salesRecordID + "  itemID=" + itemID + "quantity=" + quantity + "and countryID=" + countryID);
+        
+        try {
+            // create new entry in lineitementity and retrieve lineItemID
+            // linked with salesRecord to show which items are sold
+            Long lineItemId = db.insertLineitementity(quantity, itemID);
+            
+            // error checking
+            if (lineItemId <= 0L) {
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
+            
+            // link salesRecord and itemRecord
+            int result = db.insertSalesrecordentity_lineitementity(salesRecordID, lineItemId);
+            
+            // error checking
+            if (result == 0) {
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
+
+            // update existing item quantities and storage bin freevolume
+            db.updateQuantity(itemID, quantity, countryID);
+            
+            // return 1 for servlet error checking
+            String responseResult = "1";
+            return Response.status(Response.Status.CREATED).entity(responseResult + "").build();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
     }
 }
